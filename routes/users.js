a444de82bddd6aa9416c6c72b2bebeb9ab8b6b12
srinivasa <murthysrinivasa27@gmail.com>
2018-12-17 22:55:48 +0530

    testcase added

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,31 @@
 var express= require("express");
 var router = express.Router();
 const fs = require('fs');
+const async = require('async');
 let rawdata = fs.readFileSync('medicine.json');  
 let medData = JSON.parse(rawdata);
+var flatten = require('array-flatten')
+
+let writeDat = async function(data){
+    return await fs.writeFileSync('medicine.json', data);
+}
+
+let deletedata = function(data, id) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const index = data.findIndex(x => x.id == id);
+          data.splice(index, 1);
+        data = JSON.stringify(data, null, 2); 
+            resolve();
+        }, 1000);
+    });
+}
 
 router.get('/',function(req,res){
-    return res.json({success:true, results:medData}) ;
+    var arry =  [1, 2, [3, 4]];
+    let reducer = (accumulator, currentValue) => accumulator + currentValue;
+    let aa= arry.reduce((acc, val) => acc.concat(val), []);
+    return res.json({success:true, results: aa}) ;
 });
 
 router.post('/create',function(req,res){
@@ -32,13 +52,22 @@ router.put('/update/:id',function(req,res){
     return res.json({success:true, message:'Record updated successfully.'})
 });
 
-router.delete('/delete/:id',function(req,res){
-    let medobj = medData.medicines.find(x => x.id == req.body.id);
-    const index = medData.medicines.indexOf(medobj);
-    medData.medicines.splice(index, 1);
-    let data = JSON.stringify(medData, null, 2);    
-    fs.writeFileSync('medicine.json', data);
-    return res.json({success:true, message:'Record deleted successfully.'})
+router.delete('/delete/:id',function(req,res, next){
+    //let medobj = medData.medicines.find(x => x.id == req.params.id);
+    //const index = medData.medicines.indexOf(medobj);
+    try {
+        const index = medData.medicines.findIndex(x => x.id == req.body.id);
+        medData.medicines.splice(index, 1);
+        let data = JSON.stringify(medData, null, 2);  
+        console.log("index", index);
+        console.log(medData);  
+        //fs.writeFileSync('medicine.json', data);
+        writeDat(data);
+        return res.json({success:true, message:'Record deleted successfully.'})
+    } catch (error)
+    {
+        return next(error);
+    }
 });
 
 module.exports = router;
---
+++ test/test.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const chai = require('chai');
+const expect = require('chai').expect;
+
+chai.use(require('chai-http'));
+
+const app = require('../app.js'); // Our app
+
+describe('API endpoint /users', function() {
+  this.timeout(5000); // How long to wait for a response (ms)
+
+  before(function() {
+
+  });
+
+  after(function() {
+
+  });
+
+  // GET - List all colors
+  it('should return all data', function() {
+    return chai.request(app)
+      .get('/users')
+      .then(function(res) {
+
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body.results).to.be.an('array');
+        expect(res.body.results).to.have.lengthOf(4);
+      });
+  });
+
+  it('should delete record from data', function() {
+    return chai.request(app)
+      .delete('/users/delete/1')
+      .then(function(res) {
+
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+       
+        //expect(res.body.results).to.have.lengthOf(1);
+      });
+  });
+  
+});
